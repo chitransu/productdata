24b70211fc699e941a3a8a7250eab82c926926e8
chitransu007 <chitransu007@gmail.com>
2020-05-30 19:10:08 +0530

    first commit

---
+++ src/test/java/com/chits/springdata/product/ProductdataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chits.springdata.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductdataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
