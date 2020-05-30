0ccba535a35fb8324d2d742b2c6d48b2da24acdb
chitransu007 <chitransu007@gmail.com>
2020-05-30 20:26:14 +0530

    testing read update delete operations

--- src/test/java/com/chits/springdata/product/ProductdataApplicationTests.java
+++ src/test/java/com/chits/springdata/product/ProductdataApplicationTests.java
@@ -2,11 +2,14 @@ package com.chits.springdata.product;
 
 import com.chits.springdata.product.entities.Product;
 import com.chits.springdata.product.repos.ProductRepository;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.Optional;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -25,8 +28,32 @@ class ProductdataApplicationTests {
         product.setId(1);
         product.setName("RedMe Pro");
         product.setDesc("Good phone in low price");
-
+        product.setPrice(89.9);
         productRepository.save(product);
+    }
+
+    @Test
+    public void testRead(){
+        Optional<Product> productOptional = productRepository.findById(1);
+        if(productOptional.isPresent()){
+            System.out.println("product description "+productOptional.get().getDesc());
+            Assertions.assertEquals("RedMe Pro",productOptional.get().getName());
+        }
+    }
+
+    @Test
+    public void testUpdate(){
+        Optional<Product> productOptional = productRepository.findById(1);
+        if(productOptional.isPresent()){
+           Product product = productOptional.get();
+           product.setPrice(99.1);
+           productRepository.save(product);
+        }
+    }
+
+    @Test
+    public void testDelete(){
+        productRepository.deleteById(1);
     }
 
 }
